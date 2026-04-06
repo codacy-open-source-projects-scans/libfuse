310f9c7cee6a3881b12966322c01aa7e0273aee8
Codacy Archive <codacy-archive@local>
2026-04-06 00:23:23 +0000

    snapshot 2026-04-06T00:23:23.112381+00:00

--- lib/fuse.c
+++ lib/fuse.c
@@ -1644,7 +1644,12 @@ int fuse_fs_rename(struct fuse_fs *fs, const char *oldpath,
 
 int fuse_fs_unlink(struct fuse_fs *fs, const char *path)
 {
-	fuse_get_context()->private_data = fs->user_data;
+	struct fuse_context *ctx;
+
+	ctx = fuse_get_context();
+	if (ctx)
+		ctx->private_data = fs->user_data;
+
 	if (!fs->op.unlink)
 		return -ENOSYS;
 	if (fs->debug)
@@ -2543,7 +2548,11 @@ static struct fuse_context_i *fuse_create_context(struct fuse *f)
 			fuse_log(FUSE_LOG_ERR, "fuse: failed to allocate thread specific data\n");
 			abort();
 		}
-		pthread_setspecific(fuse_context_key, c);
+		if (pthread_setspecific(fuse_context_key, c) != 0) {
+			fuse_log(FUSE_LOG_ERR, "fuse: failed to set thread specific data\n");
+			free(c);
+			abort();
+		}
 	} else {
 		memset(c, 0, sizeof(*c));
 	}
--- lib/fuse_lowlevel.c
+++ lib/fuse_lowlevel.c
@@ -3995,12 +3995,15 @@ pipe_retry:
 				res = grow_pipe_to_max(llp->pipe[0]);
 				if (res > 0)
 					llp->size = res;
+				fuse_ll_clear_pipe(se);
 				goto fallback;
 			}
 			llp->size = res;
 		}
-		if (llp->size < bufsize)
+		if (llp->size < bufsize) {
+			fuse_ll_clear_pipe(se);
 			goto fallback;
+		}
 	}
 
 	if (se->io != NULL && se->io->splice_receive != NULL) {
@@ -4438,17 +4441,16 @@ int fuse_session_mount(struct fuse_session *se, const char *_mountpoint)
 				fd);
 			goto error_out;
 		}
-		se->fd = fd;
-		return 0;
+		goto out;
 	}
 
 	/* Open channel */
 	fd = fuse_kern_mount(mountpoint, se->mo);
 	if (fd == -1)
 		goto error_out;
-	se->fd = fd;
 
-	/* Save mountpoint */
+out:
+	se->fd = fd;
 	se->mountpoint = mountpoint;
 
 	return 0;
